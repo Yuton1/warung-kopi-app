5c8ceca4aa2204b052e8da414efb93029f38339a
Malik <mohahsanmalik1@gmail.com>
2026-04-27 18:34:39 +0700

    perbaiki space fitur

--- frontend/src/layouts/CustomerLayout.jsx
+++ frontend/src/layouts/CustomerLayout.jsx
@@ -4,9 +4,10 @@ import CustomerNavbar from '../components/CustomerNavbar'
 
 const CustomerLayout = () => {
   return (
-    <div className="app-shell">
+    <div className="app-shell flex flex-col min-h-screen">
       <CustomerNavbar />
-      <main className="app-main">
+      {/* Hapus class yang membatasi lebar di sini jika ada di CSS 'app-main' */}
+      <main className="flex-grow w-full">
         <Outlet />
       </main>
       <CustomerFooter />
