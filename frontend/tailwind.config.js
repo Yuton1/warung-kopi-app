af4fa69538c6ee08a47fb6dabc4c2ce78f89f791
Malik <mohahsanmalik1@gmail.com>
2026-04-29 21:49:41 +0700

    perbaiki ui

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        'coffee-dark': '#2C1B0E',
-        'coffee-medium': '#4B3621',
-        'coffee-light': '#A67B5B',
-        'coffee-cream': '#ECB176',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
